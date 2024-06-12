f47e4929ef89a0ad61221b79b32b32ea7e958b30
Ponomarenko Valeriya <Lera.ponomarenko.2017@mail.ru>
2024-06-12 17:47:10 +0300

    'add

---
+++ widget.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import Any
+
+import masks
+
+
+def mask_account_card(string: str) -> str:
+    """Функция общей маскировки карты и счета"""
+    if "Счет" in string:
+        account = string[-20:]
+        return string[-20:] + masks.mask_account(account)
+    else:
+        cardnumber = "".join(string[-16:].split())
+        return string[:-16] + masks.mask_card_number(cardnumber)
+
+
+def get_data(date: str) -> str:
+    """Функция преобразования даты"""
+    date_it = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+    formated_date = date_it.strptime("%d.%m.%Y")
+    return formated_date
+
